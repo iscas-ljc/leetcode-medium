48903a9805b58434cd2e203e33c853fd9352c823
brodan <861218470@qq.com>
2017-12-19 17:31:58 +0800

    101-200

---
+++ 101-200/130. Answer of Surrounded Regions.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+
+        def fill(x, y):
+            if x < 0 or x > m - 1 or y < 0 or y > n - 1 or board[x][y] != 'O':
+                return
+            queue.append((x, y))
+            board[x][y] = 'D'
+
+        def bfs(x, y):
+            if board[x][y] == 'O':
+                fill(x, y)
+            while queue:
+                cur = queue.pop(0)
+                i = cur[0]
+                j = cur[1]
+                fill(i + 1, j)
+                fill(i - 1, j)
+                fill(i, j + 1)
+                fill(i, j - 1)
+
+        if len(board) == 0:
+            return
+        m = len(board)
+        n = len(board[0])
+        queue = []
+
+        for i in range(n):
+            bfs(0, i)
+            bfs(m - 1, i)
+        for j in range(1, m - 1):
+            bfs(j, 0)
+            bfs(j, n - 1)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'D':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
