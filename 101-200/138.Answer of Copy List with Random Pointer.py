2f7690b4f129717337956d3567fbe1dadd9dfcf8
brodan <861218470@qq.com>
2017-12-21 16:11:31 +0800

    101-200

---
+++ 101-200/138.Answer of Copy List with Random Pointer.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param head, a RandomListNode
+    # @return a RandomListNode
+    def copyRandomList(self, head):
+        nodeDict = dict()
+        dummy = RandomListNode(0)
+        pointer, newHead = head, dummy
+        while pointer:
+            newNode = RandomListNode(pointer.label)
+            nodeDict[pointer] = newHead.next = newNode
+            newHead, pointer = newHead.next, pointer.next
+        pointer, newHead = head, dummy.next
+        while pointer:
+            if pointer.random:
+                newHead.random = nodeDict[pointer.random]
+            pointer, newHead = pointer.next, newHead.next
+        return dummy.next
